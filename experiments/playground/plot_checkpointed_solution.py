14b22a8fdfa3e39aab5643a1a1e7e80d7079f398
Nicholas Krämer <nicokraemer@gmx.net>
2024-06-10 11:05:34 +0200

    Plot the checkpointed solution to simplify debugging checkpoint-smoothing

--- experiments/playground/plot_checkpointed_solution.py
+++ experiments/playground/plot_checkpointed_solution.py
@@ -1,1 +1,24 @@
 """Plot the probabilistic ODE solution."""
+
+import jax
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+from odecheckpts import ivpsolvers, ivps
+
+
+vf, u0, time_span, args = ivps.rigid_body()
+
+dt0 = 0.1
+atol, rtol = 1e-4, 1e-4
+save_at = jnp.linspace(*time_span, num=100)
+u0_like = u0  # infer shapes etc.
+
+
+solve = ivpsolvers.solve_and_save_at(
+    vf, u0_like, save_at, dt0=dt0, atol=atol, rtol=rtol
+)
+solve = jax.jit(solve)
+u, solution_full = solve(u0, args)
+
+plt.plot(save_at, u)
+plt.show()
