f0730130e0e6984d7fbbe4de85f0457579c69d93
Nicholas Krämer <nicokraemer@gmx.net>
2024-06-10 10:35:20 +0200

    Make time_span an argument to rigid_body to be able to alter it

--- src/odecheckpts/ivps.py
+++ src/odecheckpts/ivps.py
@@ -13,3 +13,13 @@ def logistic():
 
     u0 = jnp.atleast_1d(u0)
     return vf, u0, time_span, args
+
+
+def rigid_body(*, time_span=(0.0, 10.0)):
+    backend.select("jax")
+    f, u0, time_span, args = ivps.rigid_body(time_span=time_span)
+
+    def vf(u, t, p):
+        return f(u, *p)
+
+    return vf, u0, time_span, args
