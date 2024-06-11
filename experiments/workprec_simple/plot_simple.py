0379a7b15c266d65b3afdc05a2123175ccd3a53e
Nicholas Krämer <nicokraemer@gmx.net>
2024-06-11 16:37:16 +0200

    Improve the code clarity for styling because we will style more soon

--- experiments/workprec_simple/plot_simple.py
+++ experiments/workprec_simple/plot_simple.py
@@ -1,6 +1,25 @@
 import os
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
+from odecheckpts import exp_util
+
+#
+# rc = {
+#     "font.size": 10,
+#     "axes.titlesize": "medium",
+#     "legend.fontsize": "small",
+#     "legend.frameon": True,
+#     "legend.facecolor": "white",
+#     "legend.edgecolor": "black",
+#     "legend.fancybox": False,
+#     "lines.linewidth": 0.75,
+#     "axes.linewidth": 0.5,
+#     "markers.fillstyle": "none",
+# }
+#
+
+plt.rcParams.update(exp_util.plot_params())
+style = exp_util.style_rigid_body()
 
 
 def load_results():
@@ -19,87 +38,83 @@ def load_timeseries():
     return jnp.load(os.path.dirname(__file__) + "/plot_timeseries.npy")
 
 
-def choose_style(label):
-    """Choose a plotting style for a given algorithm."""
-    if "int" in label:
-        style = {"linestyle": "dashed"}
-    elif "TS" in label:
-        style = {"linestyle": "solid"}
-    elif "SciPy" in label:
-        style = {"linestyle": "dashed"}
-    elif "()" in label:
-        style = {"linestyle": "dotted"}
-
-    if "2" in label or "eun" in label:
-        style["color"] = "C0"
-    if "4" in label or "opri" in label:
-        style["color"] = "C1"
-
-    return style
-
-
 def plot_results(axis, results):
     """Plot the results."""
-    axis.set_title("Work versus precision", fontsize="medium")
+    axis.set_title("Work versus precision")
     for label, wp in results.items():
         if "interp" in label:
-            label = label.replace(") (interp.", ", interp")
-        style = choose_style(label)
+            label = label.replace(") (interp.", ", interp.")
 
         precision = wp["precision"]
         work_mean, work_std = (wp["work_mean"], wp["work_std"])
 
-        axis.loglog(precision, work_mean, marker=".", label=label, **style)
+        axis.loglog(
+            precision,
+            work_mean,
+            marker=style.marker(label),
+            linestyle=style.linestyle(label),
+            label=style.label(label),
+            color=style.color(label),
+        )
 
         range_lower, range_upper = work_mean - work_std, work_mean + work_std
-        axis.fill_between(precision, range_lower, range_upper, alpha=0.3, **style)
+        axis.fill_between(
+            precision,
+            range_lower,
+            range_upper,
+            alpha=style.alpha_fill_between(label),
+            color=style.color(label),
+        )
 
     axis.set_ylim((1.1e-5, 1e1))
     axis.set_xlabel("Time-series error (RMSE)")
     axis.set_ylabel("Wall time (s)")
-    axis.grid(linestyle="dotted")
-    axis.legend(
+    axis.grid()
+    legend = axis.legend(
         ncols=3,
-        handlelength=2.6,
-        loc="lower center",
-        facecolor="ghostwhite",
-        edgecolor="black",
-        fontsize="x-small",
+        # handlelength=2.6,
+        # loc="lower center",
     )
+    legend.get_frame().set_linewidth(0.5)
     return axis
 
 
 def plot_results_error_vs_length(axis, results):
     """Plot the results."""
-    axis.set_title("Memory requirements", fontsize="medium")
+    axis.set_title("Memory requirements")
     for label, wp in results.items():
         if "TS" in label:
-            style = choose_style(label)
-
             precision = wp["precision"]
             length = wp["length_of_longest_vector"]
 
-            axis.loglog(precision, length, marker=".", label=label, alpha=0.9, **style)
+            axis.loglog(
+                precision,
+                length,
+                label=style.label(label),
+                marker=style.marker(label),
+                color=style.color(label),
+                linestyle=style.linestyle(label),
+                alpha=0.9,
+            )
 
     axis.set_ylim((0.9, 1e4))
-    axis.legend(
-        handlelength=3.0,
-        ncols=2,
-        loc="center left",
-        facecolor="ghostwhite",
-        edgecolor="black",
-        fontsize="x-small",
+    legend = axis.legend(
+        # handlelength=3.0,
+        ncols=1,
+        # loc="center left",
     )
+    legend.get_frame().set_linewidth(0.5)
+
     axis.set_xlabel("Time-series error (RMSE)")
     axis.set_ylabel("Length of solution vector")
-    axis.grid(linestyle="dotted")
+    axis.grid()
     return axis
 
 
 def plot_solution(axis, ts, ys, timeseries, yscale="linear"):
     """Plot the IVP solution."""
-    axis.set_title("Rigid body problem", fontsize="medium")
-    for colour, y in zip(["black", "darkgreen", "darkred"], ys.T):
+    axis.set_title("Rigid body problem")
+    for colour, y in zip(["black", "darkslategray", "midnightblue"], ys.T):
         axis.plot(ts, y, linewidth=0.75, linestyle="solid", color=colour, alpha=0.8)
 
     for t in timeseries:
@@ -113,12 +128,12 @@ def plot_solution(axis, ts, ys, timeseries, yscale="linear"):
 
 
 layout = [
-    ["solution", "solution", "solution", "solution"],
-    ["benchmark", "benchmark", "error_vs_length", "error_vs_length"],
-    ["benchmark", "benchmark", "error_vs_length", "error_vs_length"],
-    ["benchmark", "benchmark", "error_vs_length", "error_vs_length"],
+    ["solution", "solution", "solution", "solution", "solution"],
+    ["benchmark", "benchmark", "benchmark", "error_vs_length", "error_vs_length"],
+    ["benchmark", "benchmark", "benchmark", "error_vs_length", "error_vs_length"],
+    ["benchmark", "benchmark", "benchmark", "error_vs_length", "error_vs_length"],
 ]
-fig, axes = plt.subplot_mosaic(layout, figsize=(8, 4), constrained_layout=True, dpi=100)
+fig, axes = plt.subplot_mosaic(layout, figsize=(8, 5), constrained_layout=True, dpi=100)
 
 
 results = load_results()
@@ -130,11 +145,9 @@ _ = plot_results_error_vs_length(axes["error_vs_length"], results)
 _ = plot_solution(axes["solution"], ts, ys, timeseries)
 
 
-axes["solution"].set_title("a.", loc="left", fontsize="medium", fontweight="bold")
-axes["benchmark"].set_title("b.", loc="left", fontsize="medium", fontweight="bold")
-axes["error_vs_length"].set_title(
-    "c.", loc="left", fontsize="medium", fontweight="bold"
-)
+axes["solution"].set_title("a.", loc="left", fontweight="bold")
+axes["benchmark"].set_title("b.", loc="left", fontweight="bold")
+axes["error_vs_length"].set_title("c.", loc="left", fontweight="bold")
 
 plt.savefig(os.path.dirname(__file__) + "/figure.pdf")
 plt.show()
---
+++ src/odecheckpts/exp_util.py
@@ -0,0 +1,98 @@
+from tueplots import axes
+import dataclasses
+
+from typing import Callable
+
+
+def plot_to_pgf(filename, X, y):
+    if filename[-4:] != ".dat":
+        filename = f"{filename}.dat"
+    with open(filename, "w") as file1:
+        for x_, y_ in zip(X, y):
+            file1.write(f"{x_} {y_}\n")
+
+
+def plot_params():
+    fontsize = fontsize_uniform(10)
+    axes_lines = axes.lines(base_width=0.35)
+    axes_legend = axes.legend()
+    axes_grid = axes.grid()
+    axes_ticks = axes.tick_direction(x="in", y="in")
+    return {
+        "markers.fillstyle": "none",
+        **fontsize,
+        **axes_lines,
+        **axes_legend,
+        **axes_grid,
+        **axes_ticks,
+    }
+
+
+def fontsize_uniform(base):
+    return {
+        "font.size": base,
+        "axes.labelsize": "medium",
+        "legend.fontsize": "small",
+        "xtick.labelsize": "small",
+        "ytick.labelsize": "small",
+        "axes.titlesize": "medium",
+    }
+
+
+@dataclasses.dataclass
+class Style:
+    marker: Callable[[str], str]
+    label: Callable[[str], str]
+    color: Callable[[str], str]
+    linestyle: Callable[[str], str]
+    alpha_fill_between: Callable[[str], float]
+
+
+def style_rigid_body():
+    def marker(string, /):
+        if "can't" in string.lower() or "interp" in string.lower():
+            return "^"
+        if "bosh3" in string.lower():
+            return "s"
+        if "tsit5" in string.lower():
+            return "s"
+        return "o"
+
+    def label(string, /):
+        if "()" in string:
+            string = string.replace("()", "")
+        if "interp." in string:
+            string = string.replace("interp.", "interpolate")
+        return string
+
+    def color(string, /):
+        if "2" in string:
+            return "black"
+        if "4" in string:
+            return "darkorange"
+        if "bosh3" in string.lower():
+            return "black"
+        if "tsit5" in string.lower():
+            return "darkorange"
+        return "black"
+
+    def alpha_fill_between(_string):
+        return 0.1
+
+    def linestyle(string, /):
+        if "bosh3" in string.lower():
+            return "dotted"
+        if "tsit5" in string.lower():
+            return "dotted"
+        if "can't" in string.lower():
+            return "dashed"
+
+        return "solid"
+
+    return Style(
+        marker=marker,
+        label=label,
+        color=color,
+        alpha_fill_between=alpha_fill_between,
+        linestyle=linestyle,
+    )
