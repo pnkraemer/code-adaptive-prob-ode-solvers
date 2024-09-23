cd823c031835654b6b266293ba46f6ef1fb08a3c
Nicholas Krämer <nicokraemer@gmx.net>
2024-09-23 16:55:39 +0200

    Compile the full adaptive forward pass for this figure

--- experiments/van_der_pol/figure_1.py
+++ experiments/van_der_pol/figure_1.py
@@ -4,6 +4,7 @@ import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import time
+import contextlib
 
 from probdiffeq import ivpsolve, ivpsolvers, taylor
 from probdiffeq.impl import impl
@@ -12,55 +13,72 @@ from tueplots import axes
 plt.rcParams.update(axes.legend())
 
 
+@contextlib.contextmanager
+def time_function():
+    start = time.perf_counter()
+    yield lambda: time.perf_counter() - start
+
+
 def main():
     jax.config.update("jax_platform_name", "cpu")
     jax.config.update("jax_enable_x64", True)
-
     impl.select("dense", ode_shape=(1,))
 
     vf, (u0, du0), (t0, t1) = problem_van_der_pol()
-    init, solver = solver_ts1(vf, (u0, du0), t0)
+    init, solver = solver_ts1(vf=vf, init=(u0, du0), t0=t0)
 
-    # Pre-compile and compute error
-    _, asol_u = solve_adaptive(vf, init, (t0, t1), solver, tol=1e-3)
+    # Compute the grid to-be-used for the adaptive solver
+    grid_adaptive, sol_adaptive = solve_adaptive(
+        vf=vf, init=init, time_span=(t0, t1), solver=solver, tol=1e-3
+    )
 
-    # Execute and benchmark
-    time_ = time.perf_counter()
-    asol_t, asol_u = solve_adaptive(vf, init, (t0, t1), solver, tol=1e-3)
-    asol_u.block_until_ready()
-    asol_u.block_until_ready()
-    atime = time.perf_counter() - time_
+    # Assemble the fixed-step reference grids
+    num_steps = ((t1 - t0) / jnp.amin(jnp.diff(grid_adaptive))).astype(int)
+    grid_fixed_accurate = jnp.linspace(t0, t1, num=num_steps, endpoint=True)
+    grid_fixed_inaccurate = jnp.linspace(t0, t1, num=len(grid_adaptive), endpoint=True)
 
-    # Pre-compile
-    dtmin = jnp.amin(jnp.diff(asol_t))
-    _ = solve_fixed(vf, init, (t0, t1), solver, dtmin)
+    # Benchmark the solvers
+    with time_function() as stopwatch:
+        sol = ivpsolve.solve_fixed_grid(vf, init, grid=grid_adaptive, solver=solver)
+        sol.u.block_until_ready()
 
-    # Execute and benchmark
-    time_ = time.perf_counter()
-    fsol_t, fsol_u = solve_fixed(vf, init, (t0, t1), solver, dtmin)
-    fsol_t.block_until_ready()
-    fsol_u.block_until_ready()
-    ftime = time.perf_counter() - time_
+    time_adaptive = stopwatch()
+
+    with time_function() as stopwatch:
+        sol = ivpsolve.solve_fixed_grid(
+            vf, init, grid=grid_fixed_accurate, solver=solver
+        )
+        sol.u.block_until_ready()
+
+    time_fixed_accurate = stopwatch()
 
     fig, ax = plt.subplots(figsize=(5, 3), constrained_layout=True)
     ax.semilogy(
-        asol_t[:-1],
-        jnp.diff(asol_t),
+        grid_adaptive[:-1],
+        jnp.diff(grid_adaptive),
         linestyle="solid",
         marker="None",
         markersize=1,
         color="C0",
-        label=f"$N$={len(asol_t):,} adaptive steps in {atime:.1f} sec",
+        label=f"$N$={len(grid_adaptive):,} adaptive steps run in {time_adaptive:.1f} sec",
     )
     ax.semilogy(
-        fsol_t[:-1],
-        jnp.diff(fsol_t),
+        grid_fixed_inaccurate[:-1],
+        jnp.diff(grid_fixed_inaccurate),
         linestyle="dotted",
         marker="None",
-        color="C1",
-        label=f"$N$={len(fsol_t):,} fixed steps in {ftime:.1f} sec",
+        color="gray",
+        label=f"$N$={len(grid_fixed_inaccurate):,} fixed steps contain NaNs",
     )
-    ax.legend(loc="upper left", edgecolor="white", handlelength=1.0, fontsize="medium")
+    ax.semilogy(
+        grid_fixed_accurate[:-1],
+        jnp.diff(grid_fixed_accurate),
+        linestyle="dashed",
+        marker="None",
+        color="C1",
+        label=f"$N$={len(grid_fixed_accurate):,} fixed steps run in {time_fixed_accurate:.1f} sec",
+    )
+    ax.legend(loc="upper left", edgecolor="white", handlelength=1.5, fontsize="small")
     ax.set_xlabel(r"ODE domain (time $t$)")
     ax.set_ylabel(r"Step-size $\Delta t$")
     ax.set_ylim((2e-6, 5e1))
@@ -71,8 +89,7 @@ def main():
     axin1.set_title("VdP solution", fontsize="small")
     axin1.set_xticks((0.0, 3.0, 6.0))
     axin1.set_yticks((-2, 2))
-
-    axin1.plot(asol_t, asol_u, color="black", linewidth=0.75)
+    axin1.plot(grid_adaptive, sol_adaptive, color="black", linewidth=0.75)
     axin1.set_xlim((0.0, 6.3))
 
     filename = str(__file__)
@@ -93,7 +110,7 @@ def problem_van_der_pol():
     return vf, (u0, du0), (t0, t1)
 
 
-def solver_ts1(vf, init, t0):
+def solver_ts1(*, vf, init, t0):
     num = 4
     ibm = ivpsolvers.prior_ibm(num_derivatives=num)
     ts0 = ivpsolvers.correction_ts1(ode_order=2)
@@ -106,7 +123,7 @@ def solver_ts1(vf, init, t0):
     return init, solver
 
 
-def solve_adaptive(vf, init, time_span, solver, tol):
+def solve_adaptive(*, vf, init, time_span, solver, tol):
     ctrl = ivpsolve.control_proportional_integral()
     adaptive_solver = ivpsolve.adaptive(solver, atol=tol, rtol=tol, control=ctrl)
 
@@ -116,15 +133,6 @@ def solve_adaptive(vf, init, time_span, solver, tol):
         vf, init, t0=t0, t1=t1, dt0=dt0, adaptive_solver=adaptive_solver
     )
     return solution.t, solution.u
-
-
-def solve_fixed(vf, init, time_span, solver, dt):
-    t0, t1 = time_span
-    num_steps = ((t1 - t0) / dt).astype(int)
-
-    grid = jnp.linspace(t0, t1, num=num_steps, endpoint=True)
-    sol = ivpsolve.solve_fixed_grid(vf, init, grid=grid, solver=solver)
-    return grid, sol.u
 
 
 if __name__ == "__main__":
