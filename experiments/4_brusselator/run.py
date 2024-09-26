125bd251ba9c4138bd7356884725f444369d83b6
Nicholas Krämer <nicokraemer@gmx.net>
2024-09-26 13:33:56 +0200

    Plot the brusselator nicely

--- experiments/4_brusselator/run.py
+++ experiments/4_brusselator/run.py
@@ -14,7 +14,7 @@ def main():
     plt.rcParams.update(exp_util.plot_params())
 
     # Simulate once to get plotting code
-    N = 50
+    N = 100
     vf, u0, tspan, params = ivps.brusselator(N=N)
     solve = ivpsolvers.asolve_scipy("LSODA", vf, tspan, atol=1e-13, rtol=1e-13)
     ts, ys = solve(u0, params)
@@ -23,11 +23,15 @@ def main():
     xs = jnp.linspace(0, 1, endpoint=True, num=len(u0[0]) // 2)
     Xs, Ts = jnp.meshgrid(xs, ts)
 
-    plt.subplots(figsize=(3.25, 2.5), dpi=200)
-    plt.contourf(Xs, Ts, Us)
-    plt.xlabel("Space $x$")
-    plt.ylabel("Time $t$")
-    plt.title(f"Brusselator: $N={len(ts):,}$ adaptive steps")
+    fig, ax = plt.subplots(figsize=(3.25, 2.5), dpi=200)
+    ax.contourf(Xs, Ts, Us)
+
+    ax.set_xlabel("Space $x$")
+    ax.set_ylabel("Time $t$ (steps are marked)")
+    ax.set_title(f"Brusselator: $N={len(ts):,}$ adaptive steps")
+    ax.set_yticks(ts[::20])
+    ax.set_yticklabels(())
+    ax.tick_params(which="both", direction="out")
     plt.show()
 
     #
