5e47ae371256dc3fc52b89eb3dd3779d27f1a4dd
Nicholas Krämer <nicokraemer@gmx.net>
2024-09-26 15:13:57 +0200

    Found a parametrisation that I like

--- experiments/4_brusselator/run.py
+++ experiments/4_brusselator/run.py
@@ -10,6 +10,8 @@ from probdiffeq.impl import impl
 import jax.numpy as jnp
 from odecheckpts import exp_util
 
+import time
+
 
 def main():
     # Set up all the configs
@@ -35,7 +37,7 @@ def main():
     #
     #     print(N, 8 * 1024**3 / jax.flatten_util.ravel_pytree(init)[0].nbytes, "copies fit in memory")
 
-    for N in [4, 12, 20, 28, 32]:
+    for N in [2, 4, 8, 16, 32, 64]:
         vf, u0, (t0, t1), params = ivps.brusselator(N=N)
 
         # Set up the solver
@@ -44,21 +46,18 @@ def main():
             impl.select("dense", ode_shape=(2 * N,))
 
         num = 4
+        tol = 1e-8
+        ctrl = ivpsolve.control_proportional_integral()
         ibm = ivpsolvers.prior_ibm(num_derivatives=num)
         ts0 = ivpsolvers.correction_ts1(ode_order=1)
         strategy = ivpsolvers.strategy_fixedpoint(ibm, ts0)
         solver = ivpsolvers.solver_dynamic(strategy)
+        adaptive_solver = ivpsolve.adaptive(solver, atol=tol, rtol=tol, control=ctrl)
 
         # Set up the initial condition
-        tcoeffs = taylor.odejet_padded_scan(
-            lambda *y: vf(*y, t=t0, p=params), u0, num=num
-        )
+        tcoeffs = taylor.odejet_unroll(lambda *y: vf(*y, t=t0, p=params), u0, num=num)
         output_scale = 1.0  # or any other value with the same shape
         init = solver.initial_condition(tcoeffs, output_scale)
-
-        tol = 1e-8
-        ctrl = ivpsolve.control_proportional_integral()
-        adaptive_solver = ivpsolve.adaptive(solver, atol=tol, rtol=tol, control=ctrl)
 
         # 8 GB of memory / size of initial condition.
         # Note: adaptive steps carry around three copies
@@ -70,16 +69,41 @@ def main():
 
         # Use simulate_terminal_values to see how many steps will be taken
         # without crashing the machine.
+        count0 = time.perf_counter()
         solution = ivpsolve.solve_adaptive_terminal_values(
             vf, init, t0=t0, t1=t1, dt0=0.01, adaptive_solver=adaptive_solver
         )
+        solution.u.block_until_ready()
         ncopies = solution.num_steps * size_init / 1024**2
-        msg = f"\t{int(solution.num_steps):,} steps ({int(ncopies):,} MB) were taken.\n"
+        count1 = time.perf_counter() - count0
+        msg = f"\t{int(solution.num_steps):,} steps ({int(ncopies):,} MB) were taken in {count1:.1f}s"
         print(msg)
-        if ncopies > 2_000:
-            break
 
+        if ncopies < 4000:
+            strategy_ = ivpsolvers.strategy_smoother(ibm, ts0)
+            solver_ = ivpsolvers.solver_dynamic(strategy_)
+            adaptive_solver_ = ivpsolve.adaptive(
+                solver_, atol=tol, rtol=tol, control=ctrl
+            )
+            count0 = time.perf_counter()
+            solution = ivpsolve.solve_adaptive_save_every_step(
+                vf, init, t0=t0, t1=t1, dt0=0.01, adaptive_solver=adaptive_solver_
+            )
+            solution.u.block_until_ready()
+            count1 = time.perf_counter() - count0
+            print(f"\tAdaptive solver takes {count1:.1f}s")
+
+        save_at = jnp.linspace(t0, t1, num=100)
+        count0 = time.perf_counter()
+        solution = ivpsolve.solve_adaptive_save_at(
+            vf, init, save_at=save_at, dt0=0.01, adaptive_solver=adaptive_solver
+        )
+        solution.u.block_until_ready()
+        count1 = time.perf_counter() - count0
+        print(f"\tCheckpointed solver takes {count1:.1f}s")
+    print("\n")
     assert False
+
     # Compute a baseline solution
     solution = ivpsolve.solve_adaptive_save_every_step(
         vf, init, t0=t0, t1=t1, dt0=0.01, adaptive_solver=adaptive_solver
