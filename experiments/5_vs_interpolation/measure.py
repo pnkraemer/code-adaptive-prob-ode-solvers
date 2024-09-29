c520377d94a3afb02eb6008f24b2bdf23e038dac
Nicholas Krämer <nicokraemer@gmx.net>
2024-09-29 09:24:43 +0200

    Write a benchmark runner

--- experiments/5_vs_interpolation/measure.py
+++ experiments/5_vs_interpolation/measure.py
@@ -1,11 +1,12 @@
+import functools
 import time
-from typing import Callable, NamedTuple
+from typing import NamedTuple
 
 import jax
 import jax.numpy as jnp
 from probdiffeq import ivpsolve, ivpsolvers, taylor
 from probdiffeq.impl import impl
-import matplotlib.pyplot as plt
+
 from odecheckpts import ivps
 
 
@@ -22,27 +23,44 @@ class IVPSolution(NamedTuple):
         return len(self.steps)
 
 
-class TimeOutput(NamedTuple):
-    runtime: float
-    output: IVPSolution
+class RunnerCheckpt:
+    def __init__(self, vf, init, tspan, /, *, ode_order: int, num_derivs: int):
+        self.vf = vf
 
+        ibm = ivpsolvers.prior_ibm(num_derivatives=num_derivs)
+        ts0 = ivpsolvers.correction_ts0(ode_order=ode_order)
+        strategy = ivpsolvers.strategy_filter(ibm, ts0)
+        self.solver = ivpsolvers.solver_dynamic(strategy)
+        self.ctrl = ivpsolve.control_proportional_integral()
 
-def jit_and_time(fun: Callable) -> Callable:
-    fun = jax.jit(fun)
+        # Set up the initial condition
+        t0, t1 = tspan
+        num = num_derivs + 1 - ode_order
+        tcoeffs = taylor.odejet_padded_scan(lambda *y: vf(*y, t=t0), init, num=num)
+        output_scale = jnp.ones((2,), dtype=float)
+        self.init = self.solver.initial_condition(tcoeffs, output_scale)
 
-    def fun_wrapped():
-        out = fun()
-        out.grid.block_until_ready()
-        out.solution.block_until_ready()
+        self.solve = None
 
+    def prepare(self, *, tol, save_at):
+        solve = functools.partial(self._solve, tol=tol, save_at=save_at)
+        self.solve = jax.jit(solve)
+        return self.solve()
+
+    def _solve(self, *, tol, save_at):
+        asolver = ivpsolve.adaptive(self.solver, atol=tol, rtol=tol, control=self.ctrl)
+        solution = ivpsolve.solve_adaptive_save_at(
+            self.vf, self.init, save_at=save_at, dt0=0.01, adaptive_solver=asolver
+        )
+        return IVPSolution(grid=solution.t, solution=solution.u)
+
+    def runtime(self):
         t0 = time.perf_counter()
-        out = fun()
-        out.grid.block_until_ready()
-        out.solution.block_until_ready()
+        sol = self.solve()
+        sol.grid.block_until_ready()
+        sol.solution.block_until_ready()
         t1 = time.perf_counter()
-        return TimeOutput(runtime=t1 - t0, output=out)
-
-    return fun_wrapped
+        return t1 - t0
 
 
 def main():
@@ -50,33 +68,43 @@ def main():
     jax.config.update("jax_enable_x64", True)
 
     # Set up the IVP
-    vf, (u0, du0), (t0, t1) = ivps.three_body_restricted()
+    ivp = ivps.three_body_restricted()
 
     # Set up the solver
     impl.select("blockdiag", ode_shape=(2,))
-    num = 3
-    ibm = ivpsolvers.prior_ibm(num_derivatives=num)
-    ts0 = ivpsolvers.correction_ts0(ode_order=2)
+    baseline = solve_baseline(*ivp, tol=1e-3, ode_order=2, num_derivs=3)
+    # plt.plot(*baseline.solution.T)
+    # plt.show()
+
+    save_at = jnp.linspace(jnp.amin(baseline.grid), jnp.amax(baseline.grid))
+    checkpoint = RunnerCheckpt(*ivp, ode_order=2, num_derivs=3)
+    tols = 10.0 ** (-jnp.arange(3, 8, step=1))
+    for tol in tols:
+        _solution = checkpoint.prepare(tol=tol, save_at=save_at)
+        runtime = checkpoint.runtime()
+        print(tol, runtime)
+
+
+def solve_baseline(vf, init, tspan, /, *, tol: float, ode_order: int, num_derivs: int):
+    ibm = ivpsolvers.prior_ibm(num_derivatives=num_derivs)
+    ts0 = ivpsolvers.correction_ts0(ode_order=ode_order)
     strategy = ivpsolvers.strategy_filter(ibm, ts0)
     solver = ivpsolvers.solver_dynamic(strategy)
 
     # Set up the initial condition
-    tcoeffs = taylor.odejet_padded_scan(lambda *y: vf(*y, t=t0), [u0, du0], num=num - 1)
+    t0, t1 = tspan
+    num = num_derivs + 1 - ode_order
+    tcoeffs = taylor.odejet_padded_scan(lambda *y: vf(*y, t=t0), init, num=num)
     output_scale = jnp.ones((2,), dtype=float)
     init = solver.initial_condition(tcoeffs, output_scale)
 
     # Compute a baseline solution
-    tol = 1e-3
     ctrl = ivpsolve.control_proportional_integral()
     adaptive_solver = ivpsolve.adaptive(solver, atol=tol, rtol=tol, control=ctrl)
     solution = ivpsolve.solve_adaptive_save_every_step(
         vf, init, t0=t0, t1=t1, dt0=0.01, adaptive_solver=adaptive_solver
     )
-    baseline = IVPSolution(grid=solution.t, solution=solution.u)
-    print(baseline)
-
-    plt.plot(*baseline.solution.T)
-    plt.show()
+    return IVPSolution(grid=solution.t, solution=solution.u)
 
 
 if __name__ == "__main__":
