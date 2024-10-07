1995b87758b3fdd022ad4d9561aee8907b3ab1c6
Nicholas Krämer <nicokraemer@gmx.net>
2024-10-07 08:19:56 +0200

    Clarify the PRNG's in the NODE example

--- experiments/7_node/node.py
+++ experiments/7_node/node.py
@@ -8,7 +8,6 @@ What's the point?
 """
 
 import functools
-import matplotlib.pyplot as plt
 import equinox as eqx
 import jax
 import jax.numpy as jnp
@@ -35,57 +34,83 @@ class VanDerPol(eqx.Module):
         return jnp.asarray([xdot, ydot])
 
 
-def main():
+class NeuralODE(eqx.Module):
+    mlp: eqx.Module
+
+    def __init__(self, key, *, data_size, width_size, depth):
+        self.mlp = eqx.nn.MLP(
+            in_size=data_size,
+            out_size=data_size,
+            width_size=width_size,
+            depth=depth,
+            activation=jax.nn.softplus,
+            key=key,
+        )
+
+    @property
+    def params(self):
+        return self._params
+
+    def __call__(self, u, *, t):
+        x, y = u
+        xdot = y
+        ydot = self.params * (1 - x**2) * y - x
+        return jnp.asarray([xdot, ydot])
+
+
+def main(num_data=16, std=1e-3, num_epochs=256, num_batches=4):
     jax.config.update("jax_enable_x64", True)
     impl.select("dense", ode_shape=(2,))
 
+    # Random number generation
+    key = jax.random.PRNGKey(1)
+
     # Set up the problem
     t0, t1 = 0.0, 3.15
-    save_at = jnp.linspace(t0, t1, num=100)
-    params_true = jnp.asarray(1000.0)
-    vdp_true = VanDerPol(params_true)
+    save_at = jnp.linspace(t0, t1, num=10)
 
-    sol = solve(vdp_true, jnp.array([2.0, 0.0]), save_at=save_at)
-    plt.plot(save_at, sol.u)
-    plt.show()
+    # Sample data
+    key, subkey = jax.random.split(key, num=2)
+    mu = jax.random.uniform(key, shape=())
+    model_true = VanDerPol(mu)
+    key, subkey = jax.random.split(key, num=2)
+    generate = generate_data(model_true, save_at=save_at, key=subkey, std=std)
+    print(f"Truth: {mu:.3f}")
 
-    assert False
+    key, subkey = jax.random.split(key, num=2)
+    u0s = jax.random.uniform(subkey, shape=(num_data, 2))
+    truths = jax.vmap(generate)(u0s)
 
-    @jax.vmap
-    def generate_data(u0):
-        noise = jax.random.normal(jax.random.PRNGKey(4), shape=(len(save_at), 2))
-        return solve(vdp_true, u0, save_at=save_at).u + 1e-3 * noise
+    # Set up the optimizer
+    loss = log_likelihood(save_at=save_at, std=std)
+    loss = jax.jit(jax.value_and_grad(loss))
+    optimizer = optax.adabelief(1e-2)
 
-    num_data = 16
-    u0s = jax.random.uniform(jax.random.PRNGKey(1), shape=(num_data, 2))
-    truths = generate_data(u0s)
-
-    loss = log_likelihood(save_at=save_at)
-
-    # Use Equinox's bounded while loop for differentiability
+    # Use Equinox's bounded while loop for reverse-differentiability
     loop = functools.partial(eqx.internal.while_loop, kind="bounded", max_steps=100)
     with cfl.context_overwrite_while_loop(loop):
-        # Set up the optimizer
-        loss = jax.jit(jax.value_and_grad(loss))
-        optimizer = optax.sgd(1e-5)
-        print(f"Truth: {loss(vdp_true, u0s, truths)[0]:.2e}, p={vdp_true.params}")
-
         # Initialise the optimizer
-        r0 = jnp.asarray(900.0)
+        r0 = jnp.asarray(0.1)
         vdp = VanDerPol(r0)
         opt_state = optimizer.init(vdp)
 
         # Run the training loop
-        num_epochs = 256
-        data = dataloader(u0s, truths, num_epochs=num_epochs, num_batches=4)
+        data = dataloader(u0s, truths, num_epochs=num_epochs, num_batches=num_batches)
         for idx, (inputs, outputs) in enumerate(data):
             val, grads = loss(vdp, inputs, outputs)
             updates, opt_state = optimizer.update(grads, opt_state)
             vdp = optax.apply_updates(vdp, updates)
 
-            print(f"{grads._params:.1e}")
-            label = f"{idx}/{num_epochs} | loss: {val:.2e}, p={vdp.params}"
+            label = f"{idx}/{num_epochs} | loss: {val:.2e}, p={vdp.params:.3f}"
             print(label)
+
+
+def generate_data(model_true, *, save_at, key, std):
+    def generate(u0):
+        noise = jax.random.normal(key, shape=(len(save_at), 2))
+        return solve(model_true, u0, save_at=save_at).u + std * noise
+
+    return generate
 
 
 def dataloader(inputs, outputs, /, *, num_epochs, num_batches):
@@ -98,7 +123,7 @@ def dataloader(inputs, outputs, /, *, num_epochs, num_batches):
             yield inputs[i], outputs[i]
 
 
-def log_likelihood(save_at):
+def log_likelihood(*, save_at, std):
     def loss(ode, u0s, truths):
         losses = loss_single(ode, u0s, truths)
         return jnp.mean(losses)
@@ -106,12 +131,11 @@ def log_likelihood(save_at):
     @functools.partial(jax.vmap, in_axes=(None, 0, 0))
     def loss_single(ode, u0, truth):
         solution = solve(ode, u0, save_at=save_at)
-
-        std = 1e-3 * jnp.ones_like(save_at)
+        std_vec = std * jnp.ones_like(save_at)
 
         lml = stats.log_marginal_likelihood
         lml = functools.partial(lml, posterior=solution.posterior)
-        lml = functools.partial(lml, standard_deviation=std)
+        lml = functools.partial(lml, standard_deviation=std_vec)
         return -lml(truth)
 
     return loss
@@ -119,8 +143,8 @@ def log_likelihood(save_at):
 
 def solve(ode, u0s, save_at):
     # any high-accuracy solution works. why? loss-landscape (zoom in!)
-    num = 4
-    tol = 1e-8
+    num = 3
+    tol = 1e-4
 
     # Set up the solver
     ibm = ivpsolvers.prior_ibm(num_derivatives=num)
@@ -135,14 +159,13 @@ def solve(ode, u0s, save_at):
     output_scale = 1e0  # or any other value with the same shape
     init = solver.initial_condition(tcoeffs, output_scale)
 
-    # return ivpsolve.solve_fixed_grid(ode, init, grid=save_at, solver=solver)
-
-    # Compute a baseline solution
+    # Build the solver and solve
     ctrl = ivpsolve.control_proportional_integral()
     adaptive_solver = ivpsolve.adaptive(solver, atol=tol, rtol=tol, control=ctrl)
-    return ivpsolve.solve_adaptive_save_at(
-        ode, init, save_at=save_at, dt0=0.1, adaptive_solver=adaptive_solver
-    )
+    solve_fun = functools.partial(ivpsolve.solve_adaptive_save_at, save_at=save_at)
+    solve_fun = functools.partial(solve_fun, dt0=0.1)
+    solve_fun = functools.partial(solve_fun, adaptive_solver=adaptive_solver)
+    return solve_fun(ode, init)
 
 
 if __name__ == "__main__":
